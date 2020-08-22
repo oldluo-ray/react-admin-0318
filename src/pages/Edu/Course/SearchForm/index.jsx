589880af5660687a8457b97266520281129da932
old-luo <luodianlei7@163.com>
2020-08-22 16:54:27 +0800

    多级联动完成

--- src/pages/Edu/Course/SearchForm/index.jsx
+++ src/pages/Edu/Course/SearchForm/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Form, Input, Select, Cascader, Button } from 'antd'
-import { reqAllSubjectList } from '@api/edu/subject'
+import { reqAllSubjectList, reqGetSecSubject } from '@api/edu/subject'
 import { reqGetAllTeacherList } from '@api/edu/teacher'
 
 import './index.less'
@@ -12,6 +12,7 @@ function SearchForm() {
 
   const [subjects, setSubjects] = useState([])
   const [teachers, setTeachers] = useState([])
+  const [options, setOptions] = useState([])
 
   // 模拟挂载的生命周期钩子函数
   useEffect(() => {
@@ -24,10 +25,23 @@ function SearchForm() {
         reqAllSubjectList(),
         reqGetAllTeacherList()
       ])
-
-      // console.log(subject, teacher)
       setSubjects(subject)
       setTeachers(teacher)
+      // 注意:setSubjects(subject) 执行完,不代表subjects修改完毕了
+      const optionList = subject.map(item => {
+        return {
+          // 相当于_id
+          value: item._id,
+          // 相当于title
+          label: item.title,
+          // 是否有二级数据. false表示有, true表示没有
+          isLeaf: false
+        }
+      })
+
+      // console.log(subject, teacher)
+
+      setOptions(optionList)
     }
     fetchData()
   }, [])
@@ -45,41 +59,43 @@ function SearchForm() {
   //   }
   // ])
 
-  const options = subjects.map(item => {
-    return {
-      // 相当于_id
-      value: item._id,
-      // 相当于title
-      label: item.title,
-      // 是否有二级数据. false表示有, true表示没有
-      isLeaf: true
-    }
-  })
   const onChange = (value, selectedOptions) => {
-    console.log(value, selectedOptions)
+    console.log('多级联动', value, selectedOptions)
   }
 
-  const loadData = selectedOptions => {
+  const loadData = async selectedOptions => {
+    // selectedOptions 记录了一级的数据和后面的其他级的数据
+    // loadData什么时候触发: 只有有下一级数据就可以触发(有箭头就表示有下一级数据)
+    // console.log(selectedOptions)
+    // 永远获取最后一级的数据,因为要根据最后获取下一级数据
     const targetOption = selectedOptions[selectedOptions.length - 1]
+    // 展示一个加载效果
     targetOption.loading = true
-
     // load options lazily
-    setTimeout(() => {
-      targetOption.loading = false
-      targetOption.children = [
-        {
-          label: `${targetOption.label} Dynamic 1`,
-          value: 'dynamic1'
-        },
-        {
-          label: `${targetOption.label} Dynamic 2`,
-          value: 'dynamic2'
+    // 发送异步请求获取数据
+    // 获取二级数据
+    // console.log(targetOption)
+    const res = await reqGetSecSubject(targetOption.value)
+    // 让加载效果隐藏
+    targetOption.loading = false
+    console.log(res)
+    if (res.items.length) {
+      //说明有数据,给targetOption添加children属性
+      targetOption.children = res.items.map(item => {
+        return {
+          value: item._id,
+          label: item.title
+          // 注意: 因为我们知道二级后面没有数据了,所以直接不写isLeaf. 不写默认值就是true
         }
-      ]
-      // setOptions([...options])
-    }, 1000)
-  }
+      })
+    } else {
+      // 没有拿到数据,就不填加children属性, 并且将isLeaf改为true.把小箭头去掉
+      targetOption.isLeaf = true
+    }
 
+    // 调用setOptions ,修改值让视图重新渲染
+    setOptions([...options])
+  }
   const resetForm = () => {
     form.resetFields()
   }
@@ -109,9 +125,12 @@ function SearchForm() {
         <Cascader
           style={{ width: 250, marginRight: 20 }}
           options={options}
+          // 点击菜单,如果有下一级数据,就会触发这个函数,一般在这个函数中获取下一级数据
           loadData={loadData}
+          // 可以获取到选中的数据
           onChange={onChange}
-          changeOnSelect
+          // changeOnSelect 值为true时,只要顶级了多级联动,就触发onchange. 为false时,只有选中之后才会触发
+          // changeOnSelect = {true}
           placeholder='课程分类'
         />
       </Form.Item>
