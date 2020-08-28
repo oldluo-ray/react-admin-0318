7d72cde7fc00285b8de9625e8bde116937c00a34
old-luo <luodianlei7@163.com>
2020-08-28 11:36:25 +0800

    动态展示右侧内容完成

--- src/layouts/PrimaryLayout/index.jsx
+++ src/layouts/PrimaryLayout/index.jsx
@@ -11,6 +11,9 @@ import {
   MenuFoldOutlined
 } from '@ant-design/icons'
 
+import { connect } from 'react-redux'
+
+import { withRouter } from 'react-router-dom'
 import SiderMenu from '../SiderMenu'
 
 import './index.less'
@@ -19,8 +22,9 @@ import logo from '@assets/images/logo.png'
 
 const { Header, Content, Footer, Sider } = Layout
 const { SubMenu } = Menu
-
-export default class PrimaryLayout extends Component {
+@withRouter
+@connect(state => ({ user: state.user }))
+class PrimaryLayout extends Component {
   state = {
     collapsed: false
   }
@@ -30,7 +34,43 @@ export default class PrimaryLayout extends Component {
     this.setState({ collapsed })
   }
   render() {
-    console.log(this.props.user)
+    // console.log(this.props.user)
+    const { name, avatar, permissionList } = this.props.user
+
+    // 获取地址栏的路径
+    const pathname = this.props.location.pathname
+    // /edu/subjet/list
+    // /account/settings
+    // /
+
+    // 注意: 如果正则后面不加g. 那么值返回匹配到的第一个值. 如果加了g.表示全局匹配.返回所有匹配项
+    const reg = /[/][a-z]*/g
+
+    const matchArr = pathname.match(reg)
+
+    // 拼接path路径,然后根据路径找对应名称
+    const firstPath = matchArr[0]
+    const SecPath = matchArr[1]
+    const thirdPath = matchArr[2] || ''
+
+    // 根据路径,找名字
+    let firstName
+    let SecName
+    SecPath &&
+      permissionList.forEach(route => {
+        if (route.path === firstPath) {
+          firstName = route.name
+
+          route.children.forEach(secRoute => {
+            if (secRoute.path === SecPath + thirdPath) {
+              SecName = secRoute.name
+            }
+          })
+        }
+      })
+
+    console.log(firstName, SecName)
+
     return (
       <Layout className='layout'>
         <Sider
@@ -65,17 +105,22 @@ export default class PrimaryLayout extends Component {
 
         <Layout className='site-layout'>
           <Header className='layout-header'>
-            <img src={logo} alt='' />
-            <span>用户名</span>
+            <img src={avatar} alt='' />
+            <span>{name}</span>
             <GlobalOutlined />
           </Header>
           <Content>
             <div className='layout-nav'>
-              <Breadcrumb>
-                <Breadcrumb.Item>User</Breadcrumb.Item>
-                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-              </Breadcrumb>
-              <div>提示文字</div>
+              {!firstName ? (
+                <div>首页</div>
+              ) : (
+                <Breadcrumb>
+                  <Breadcrumb.Item>{firstName}</Breadcrumb.Item>
+                  <Breadcrumb.Item>{SecName}</Breadcrumb.Item>
+                </Breadcrumb>
+              )}
+
+              <div>{SecName}</div>
             </div>
 
             <div className='layout-content'>Bill is a cat.</div>
@@ -88,3 +133,5 @@ export default class PrimaryLayout extends Component {
     )
   }
 }
+
+export default PrimaryLayout
